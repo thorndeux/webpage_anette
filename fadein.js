db8084cf2444a184c73d90e1b43cb4fdabe2465d
thorndeux <thorndeux@gmx.ch>
2020-05-27 14:06:22 +0000

    Revamped brand and navbar, changed About Me intext pic, changed color of Contact links

--- fadein.js
+++ fadein.js
@@ -2,28 +2,28 @@ var img = document.querySelector("picture");
 
 // Fades in an element by animating opacity from 0 to 1
 function fadein() {
-    $(".fadein").css("opacity", "0");
-    $(".fadein").animate({opacity: 1}, 1400);
+  $(".fadein").css("opacity", "0");
+  $(".fadein").animate({opacity: 1}, 1400);
 }
 
 // Runs when the document is ready
 $(document).ready(function(){
-    // If the image is fully loaded, run fadein function
-    if (img.complete) {
-        fadein();
-    }
-    // Else, listen for the 'load' event and run fadein function then
-    else {
-        img.addEventListener("load", fadein());
-    }
+  // If the image is fully loaded, run fadein function
+  if (img.complete) {
+    fadein();
+  }
+  // Else, listen for the 'load' event and run fadein function then
+  else {
+    img.addEventListener("load", fadein());
+  }
 });
 
 // Runs when the window is resized (e.g. orientation change on a mobile device)
 $(window).resize(function(){
-    if (img.complete) {
-        fadein();
-    }
-    else {
-        img.addEventListener("load", fadein());
-    }
+  if (img.complete) {
+    fadein();
+  }
+  else {
+    img.addEventListener("load", fadein());
+  }
 });
