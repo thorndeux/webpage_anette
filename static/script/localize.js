39f5c4325ec5fc3f674ce21f9c7a30779da37876
thorndeux <thorndeux@gmx.ch>
2021-01-27 16:35:24 +0100

    - Added localized titles and description - Added canonical URLs - Added missing image alt

--- static/script/localize.js
+++ static/script/localize.js
@@ -43,6 +43,7 @@ function localize(language) {
             $(this).hide();
         }
     });
+
     // Toggle the 'selected' property for the selected language
     $("option").each(function() {
         if ($(this).attr("value") === language) {
@@ -53,14 +54,71 @@ function localize(language) {
             $(this).attr({"selected": false, "aria-selected": false});
         }
     });
+    
+    // Array of routes, English title start, German title start, German description, English description
+    const titles = [["/",
+                     "Home", "Home",
+                     "Ich biete Behandlungen in der ganzheitlichen Körpertherapie nach Marion Rosen an. Durch sanfte, achtsame Berührung kann es zu einer tiefen Entspannung kommen. Gefühle, die im Körpergedächtnis gespeichert waren, können sich lösen und es kann sich neue Vitalität, Lebendigkeit und Zuversicht einstellen.",
+                     "Rosen-Method bodywork is a holistic type of bodywork that is simple yet powerful. With gentle, non-intrusive touch and attuned presence I help my clients to release chronic tension, contact deep relaxation, and find new vitality and ease."],
+                    ["/rosen",
+                     "Rosen-Method", "Rosen-Methode",
+                     "Den Körper berühren, die Seele erreichen. Die Rosen-Methode ist eine ganzheitliche Körpertherapie. Durch achtsame, sanfte Berührung kann es zu einer tiefen Entspannung, Vitalität und mehr Akzeptanz und Selbst-Liebe kommen.",
+                     "Touching the body, reaching the soul. Rosen-Method is a unique form of bodywork that is simple yet powerful. Through gentle and non-intrusive touch clients can release chronic tension, contact deep relaxation, and find new vitality."],
+                    ["/session",
+                     "Session", "Behandlung",
+                     "Ich biete einstündige Behandlungen in der Körperarbeit nach Marion Rosen an. Durch sanfte Berührung chronisch angespannter Muskeln kann es zu einem freieren Atem und einer tiefen Entspannung kommen. Emotionen dürfen gefühlt und ins Jetzt integriert werden.",
+                     "I offer hour-long Rosen-Method bodywork sessions. Gentle, non-manipulative touch can lead to the release of chronic tension and to emotional healing. Hidden potential is awakened, allowing for more aliveness, vitality, and possibilities."],
+                    ["/about",
+                     "About Me: Anette Koegler", "Über Mich: Anette Kögler",
+                     "Die Arbeit mit der Rosen-Methode macht mir viel Freude. Sie passt mit ihrer sanften und respektvollen Berührung wunderbar zu meiner Arbeit als Entspannungstherapeutin und Mindfulness Coach.",
+                     "I enjoy helping my clients with Rosen-Method bodywork sessions. With its gentle and mindful touch it complements my work as a Relaxation Teacher and Mindfulness Coach."],
+                    ["/contact",
+                     "Contact", "Kontakt",
+                     "Buchen Sie Ihre erste Behandlung: Ich freue mich darauf, Sie mit Mitgefühl und Achtsamkeit auf Ihrer Reise zu sich selbst zu begleiten!",
+                     "Booking your first session is easy: I am looking forward to supporting you with compassion and mindfulness on your journey home to yourself!"],
+                    ["/credits",
+                     "Credits", "Credits",
+                     "Bildnachweis und Informationen zu Webdesign und Programmierung.",
+                     "Image, web design, and programming credits."],
+                    ["/impressum",
+                     "Impressum", "Impressum",
+                     "Rechtliche Informationen zu Verantwortlichen.",
+                     ""],
+                    ["/privacy",
+                     "Privacy", "Datenschutz",
+                     "Informationen zu Datenschutz und Cookies.",
+                     "Our privacy and cookie policies."]]
+    
     // Set appropriate title (this cannot be set through display value)
     $("title").text(function() {
-        if (language === "en"){
-            return "FullyBeing Rosen Method";
+        const route = window.location.pathname
+        for (const element of titles) {
+            if (element[0] === route) {
+                if (language === "en"){
+                    return element[1] + " | FullyBeing Rosen-Method Bodywork";
+                }
+                else {
+                    return element[2] + " | FullyBeing Rosen-Methode Körperarbeit";
+                }
+            }
         }
-        else {
-            return "FullyBeing Rosen Methode";
+        return "FullyBeing Rosen-Method Bodywork"
+    });
+
+    // Set appropriate description (this cannot be set through display value)
+    $("meta[name='description']").attr("content", function() {
+        const route = window.location.pathname
+        for (const element of titles) {
+            if (element[0] === route) {
+                if (language === "en"){
+                    return element[4];
+                }
+                else {
+                    return element[3];
+                }
+            }
         }
+        return ""
     });
 
     // Localize form placeholders
